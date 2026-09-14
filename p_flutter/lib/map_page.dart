3161263a71f723a47fd8773e80bc4e42214c2ab2
Abdou <seck.abdou-samath@ugb.edu.sn>
2026-09-14 11:43:12 +0200

    ma possiton

--- p_flutter/lib/map_page.dart
+++ p_flutter/lib/map_page.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  MapLibreMapController? mapController;
+
+  Future<void> allerMaPosition() async {
+    Position position = await Geolocator.getCurrentPosition();
+
+    LatLng maPosition = LatLng(
+      position.latitude,
+      position.longitude,
+    );
+
+    // Déplacer la caméra
+    await mapController?.animateCamera(
+      CameraUpdate.newLatLngZoom(
+        maPosition,
+        16,
+      ),
+    );
+
+    // Ajouter un cercle à ma position
+    await mapController?.addCircle(
+      CircleOptions(
+        geometry: maPosition,
+        circleRadius: 10,
+        circleColor: '#FF0000',
+        circleOpacity: 0.8,
+        circleStrokeWidth: 3,
+        circleStrokeColor: '#FFFFFF',
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +47,29 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Ma carte'),
       ),
-      body: MapLibreMap(
-        styleString:
-      'https://tiles.openfreemap.org/styles/bright',
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: allerMaPosition,
+            child: const Text('📍 Ma position'),
+          ),
 
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(42.635, 8.937),
-          zoom: 7,
-           tilt: 45,
-        ),
+          Expanded(
+            child: MapLibreMap(
+              styleString:
+                  'https://tiles.openfreemap.org/styles/bright',
+
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(42.635, 8.937),
+                zoom: 7,
+              ),
+
+              onMapCreated: (controller) {
+                mapController = controller;
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
