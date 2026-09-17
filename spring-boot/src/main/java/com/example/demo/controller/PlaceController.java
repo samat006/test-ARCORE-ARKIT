a5b359e1542e42aa4ed4dce1b07e3c5fbe3dd04b
Abdou <seck.abdou-samath@ugb.edu.sn>
2026-09-17 17:14:51 +0200

    CRED

--- spring-boot/src/main/java/com/example/demo/controller/PlaceController.java
+++ spring-boot/src/main/java/com/example/demo/controller/PlaceController.java
@@ -1,14 +1,64 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Place;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PlaceController {
 
+    private List<Place> places = new ArrayList<>();
+
     @GetMapping("/places")
-    public Place getPlaces() {
-        return new Place("L'Île-Rousse", 42.633, 8.937);
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    @GetMapping("/places/{id}")
+    public Place getPlace(@PathVariable int id) {
+        for (Place place : places) {
+            if (place.getId() == id) {
+                return place;
+            }
+        }
+
+        return null;
+    }
+
+    @PostMapping("/places")
+    public Place createPlace(@RequestBody Place place) {
+        places.add(place);
+        return place;
+    }
+
+    @PutMapping("/places/{id}")
+    public Place updatePlace(
+            @PathVariable int id,
+            @RequestBody Place updatedPlace) {
+
+        for (Place place : places) {
+            if (place.getId() == id) {
+                place.setName(updatedPlace.getName());
+                place.setLatitude(updatedPlace.getLatitude());
+                place.setLongitude(updatedPlace.getLongitude());
+
+                return place;
+            }
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/places/{id}")
+    public void deletePlace(@PathVariable int id) {
+        places.removeIf(place -> place.getId() == id);
     }
 }
